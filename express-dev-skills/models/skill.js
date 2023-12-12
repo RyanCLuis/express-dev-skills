2c14d826be0e24c25414e87fc8d1686b87721f2a
Ryan Luis <RyanCLuis@gmail.com>
2023-12-12 06:25:44 -0800

    links pages together, can see /skills on local host

---
+++ express-dev-skills/models/skill.js
@@ -0,0 +1,13 @@
+var skills = [
+    {id: 125223, skill: 'Learn Html', helpful: true},
+    {id: 127904, skill: 'Learn CSS', helpful: true},
+    {id: 139608, skill: 'Learn JavaScript', helpful: true}
+]
+	
+module.exports = {
+    getAll
+}
+	
+function getAll() {
+    return skills
+}
